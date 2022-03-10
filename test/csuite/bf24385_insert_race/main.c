58d4e77c9b67d7e6b0432642e2df555fb71c07b2
Alexey Anisimov <alexey.anisimov@mongodb.com>
2022-03-10 20:55:04 +1100

    Updated test to insert random records by more threads. Plus some other minor changes.

--- test/csuite/bf24385_insert_race/main.c
+++ test/csuite/bf24385_insert_race/main.c
@@ -27,13 +27,10 @@
  */
 #include "test_util.h"
 
-#define NUM_RECORDS 1000000
-#define NUM_THREADS 20
+#define THREAD_NUM_ITERATIONS 200000
+#define NUM_THREADS 110
+#define KEY_MAX UINT32_MAX
 
-typedef struct {
-    TEST_OPTS *opts;
-    uint64_t thread_dx;
-} TEST_OPTS_INSERT;
 /*
  * JIRA ticket reference: TODO
  */
@@ -41,6 +38,7 @@ typedef struct {
 void *thread_insert_race(void *);
 
 static uint64_t ready_counter;
+static WT_RAND_STATE rnd;
 
 /*
  * set_key --
@@ -69,7 +67,7 @@ set_value(TEST_OPTS *opts, WT_CURSOR *c, uint64_t value)
  * get_value --
  *     Wrapper providing the correct typing for the WT_CURSOR::get_value variadic argument.
  */
-static uint64_t
+/*static uint64_t
 get_value(TEST_OPTS *opts, WT_CURSOR *c)
 {
     uint64_t value64;
@@ -82,7 +80,7 @@ get_value(TEST_OPTS *opts, WT_CURSOR *c)
         testutil_check(c->get_value(c, &value64));
         return (value64);
     }
-}
+}*/
 
 /*
  * main --
@@ -92,20 +90,16 @@ int
 main(int argc, char *argv[])
 {
     TEST_OPTS *opts, _opts;
-    WT_CURSOR *c;
     WT_SESSION *session;
     clock_t ce, cs;
-    pthread_t id[100];
+    pthread_t id[NUM_THREADS];
     int i, ret;
     char tableconf[128];
-    TEST_OPTS_INSERT opt_ins[NUM_THREADS];
-    uint64_t val, prev_val;
-    bool prev_val_valid = false;
+    WT_CURSOR *cursor;
 
     opts = &_opts;
     memset(opts, 0, sizeof(*opts));
     opts->nthreads = NUM_THREADS;
-    opts->nrecords = NUM_RECORDS;
     opts->table_type = TABLE_ROW;
     testutil_check(testutil_parse_opts(argc, argv, opts));
     testutil_make_work_dir(opts->home);
@@ -114,18 +108,18 @@ main(int argc, char *argv[])
       "create,cache_size=2G,eviction=(threads_max=5),statistics=(fast)", &opts->conn));
     testutil_check(opts->conn->open_session(opts->conn, NULL, NULL, &session));
     testutil_check(__wt_snprintf(tableconf, sizeof(tableconf),
-      "key_format=%s,value_format=%s,leaf_page_max=32k,", opts->table_type == TABLE_ROW ? "Q" : "r",
+      "key_format=%s,value_format=%s,leaf_page_max=32k,memory_page_image_max=50MB", opts->table_type == TABLE_ROW ? "Q" : "r",
       opts->table_type == TABLE_FIX ? "8t" : "Q"));
     testutil_check(session->create(session, opts->uri, tableconf));
-
+    
+    __wt_random_init_seed((WT_SESSION_IMPL *)session, &rnd);
+    
     cs = clock();
     
     /* Multithreaded insert */
-    for (i = 0; i < (int)opts->nthreads; ++i) {
-        opt_ins[i].opts = opts;
-        opt_ins[i].thread_dx = (uint64_t)i;
-        testutil_check(pthread_create(&id[i], NULL, thread_insert_race, &opt_ins[i]));
-    }
+    for (i = 0; i < (int)opts->nthreads; ++i)
+        testutil_check(pthread_create(&id[i], NULL, thread_insert_race, opts));
+
     while (--i >= 0)
         testutil_check(pthread_join(id[i], NULL));
 
@@ -133,31 +127,20 @@ main(int argc, char *argv[])
     testutil_check(opts->conn->close(opts->conn, NULL));
     opts->conn = NULL;
     testutil_check(wiredtiger_open(opts->home, NULL,
-      "create,cache_size=2G,eviction=(threads_max=5),statistics=(fast)", &opts->conn));
+      "create,cache_size=2G,eviction=(threads_max=1),statistics=(fast)", &opts->conn));
 
     /* Validate */
     testutil_check(opts->conn->open_session(opts->conn, NULL, NULL, &session));
     testutil_check(session->verify(session, opts->uri, NULL));
 
-    testutil_check(session->open_cursor(session, opts->uri, NULL, NULL, &c));
+    testutil_check(session->open_cursor(session, opts->uri, NULL, NULL, &cursor));
     i = 0;
-    while ((ret = c->next(c)) == 0) {
-        val = get_value(opts, c);
-        if (prev_val_valid)
-            testutil_assert(val == (prev_val + 1));
-        else
-            testutil_assert(val == 1);
-
-        prev_val = val;
-        prev_val_valid = true;
+    while ((ret = cursor->next(cursor)) == 0)
         i++;
-    }
-
     testutil_assert(ret == WT_NOTFOUND);
-    testutil_assert(i == NUM_RECORDS);
 
     ce = clock();
-    printf("Number of records: %" PRIu64 "\nDuration: %.2lf\n", opts->nrecords, (ce - cs) / (double)CLOCKS_PER_SEC);
+    printf("Number of records: %" PRIu64 "\nDuration: %.2lf\n", (uint64_t)i, (ce - cs) / (double)CLOCKS_PER_SEC);
 
     testutil_cleanup(opts);
     return (EXIT_SUCCESS);
@@ -168,23 +151,17 @@ main(int argc, char *argv[])
  *     Append to a table in a "racy" fashion - that is attempt to insert the same record another
  *     thread is likely to also be inserting.
  */
-void *
+WT_THREAD_RET
 thread_insert_race(void *arg)
 {
-    TEST_OPTS_INSERT *opts_insert;
     TEST_OPTS *opts;
     WT_CONNECTION *conn;
     WT_CURSOR *cursor;
-    WT_DECL_RET;
     WT_SESSION *session;
-    uint64_t i, ready_counter_local, thread_dx;
+    uint64_t i, ready_counter_local, key;
 
-    opts_insert = (TEST_OPTS_INSERT *)arg;
-    opts = opts_insert->opts;
-    thread_dx = opts_insert->thread_dx;
+    opts = (TEST_OPTS *)arg;
     conn = opts->conn;
-
-    printf("Running insert thread\n");
 
     testutil_check(conn->open_session(conn, NULL, NULL, &session));
     testutil_check(session->open_cursor(session, opts->uri, NULL, NULL, &cursor));
@@ -197,21 +174,12 @@ thread_insert_race(void *arg)
             break;
     }
 
-    for (i = 0; i < opts->nrecords; i += opts->nthreads) {
-        testutil_check(session->begin_transaction(session, "isolation=snapshot"));
-
-        set_key(cursor, i + thread_dx + 1);
-        set_value(opts, cursor, i + thread_dx + 1);
-        
-        if ((ret = cursor->insert(cursor)) != 0) {
-            if (ret == WT_ROLLBACK) {
-                testutil_check(session->rollback_transaction(session, NULL));
-                i--;
-                continue;
-            }
-            printf("Error in insert: %d\n", ret);
-        }
-        testutil_check(session->commit_transaction(session, NULL));
+    for (i = 0; i < THREAD_NUM_ITERATIONS; ++i) {
+        /* Generate random values from [1, KEY_MAX] */
+        key = ((uint64_t)__wt_random(&rnd) % KEY_MAX) + 1;
+        set_key(cursor, key);
+        set_value(opts, cursor, key);
+        testutil_check(cursor->insert(cursor));
     }
 
     return (NULL);
